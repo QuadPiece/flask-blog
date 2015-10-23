8a29995328a3c4b69b833bc5b26d377642a3c291
Dominik V. Salonen <domen178@gmail.com>
2015-10-23 12:52:36 +0200

    Add basic database crap

--- exec.py
+++ exec.py
@@ -1,8 +1,24 @@
-from flask import Flask
+# Import party
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+
+#Config
+DATABASE = './blog.db'
+DEBUG = True
+SECRET_KEY = 'sample key'
+USERNAME = 'user'
+PASSWORD = 'bananacar'
+
+# Create instance
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+# Database functions
+def db_connect():
+  return sqlite3.connect(app.config['DATABASE'])
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+  return 'Hello World!'
 
 app.run(debug=True)
