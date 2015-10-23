aa44086e7ec4cd73dce0a2b25ff53c53be347910
Dominik V. Salonen <domen178@gmail.com>
2015-10-23 20:41:08 +0200

    Steal a bunch of shit from a guide

--- exec.py
+++ exec.py
@@ -16,8 +16,52 @@ app.config.from_object(__name__)
 def db_connect():
   return sqlite3.connect(app.config['DATABASE'])
 
+@app.before_request
+def before_request():
+    g.db = db_connect()
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+# Actual routes
 @app.route('/')
-def index():
-  return 'Hello World!'
+def home():
+    cur = g.db.execute('select title, text from entries order by id desc')
+    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    return render_template('home.html', entries=entries)
+
+@app.route('/add', methods=['POST'])
+def add_post():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('insert into entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    g.db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('home'))
+
+# Sessions
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('home'))
 
 app.run(debug=True,host="0.0.0.0",port=5800)
